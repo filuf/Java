1dbc5be289b3810df061ab8584df6213ad34a93f
filuf <158043915+filuf@users.noreply.github.com>
2024-05-04 01:07:58 +0200

    Aprendiendo Comparator

---
+++ AprendiendoFueraDeClase/comparators/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //creamos nuestro Comparator en el que definimos el criterio de comparación
+        Comparator<Productos> comparaPorPreciosUsandoReferencia = Comparator.comparing(Productos::getPrecio);
+        Comparator<Productos> comparaPorPreciosUsandoLambda = (p1,p2) -> p1.getPrecio().compareTo(p2.getPrecio());
+
+        List<Productos> listaProductos = new ArrayList<>(List.of(
+                new Productos("Cacahuetes", 5.99),
+                new Productos("Manzana", 2.99),
+                new Productos("Sardinas", 10.95)
+        ));
+
+        //ordenamos nuestra lista
+        listaProductos.sort(comparaPorPreciosUsandoReferencia);
+
+        //mostramos los productos ordenados por preciode menor a mayor
+        listaProductos.forEach(System.out::println);
+
+        //creamos otro comparator pero esta vez de orden inverso
+        Comparator<Productos> comparaPorPreciosMayorAMenor = Comparator.comparing(Productos::getPrecio).reversed();
+
+        //ordenamos inversamente nuestra lista
+        listaProductos.sort(comparaPorPreciosMayorAMenor);
+
+        //mostramos solo el nombre y los precios
+        listaProductos.forEach(p -> System.out.println("Nombre producto - "+p.getNOMBRE_PRODUCTO()
+                                                        +"\nPrecio - "+p.getPrecio()));
+    }
+}
